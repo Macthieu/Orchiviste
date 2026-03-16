57dc3c90fb4496d63c6b281204e45d6922972353
Mathieu Beaudin <macthieu@me.com>
2026-03-16 17:25:07 -0400

    build(toolchain): migrate OrchivisteWorker to Swift 6 and macOS 14

--- OrchivisteWorker/Package.swift
+++ OrchivisteWorker/Package.swift
@@ -1,9 +1,9 @@
-// swift-tools-version: 5.9
+// swift-tools-version: 6.0
 import PackageDescription
 
 let package = Package(
     name: "OrchivisteWorker",
-    platforms: [.macOS(.v13)],
+    platforms: [.macOS(.v14)],
     products: [.executable(name: "OrchivisteWorker", targets: ["OrchivisteWorker"])],
     dependencies: [
         .package(url: "https://github.com/Mordil/RediStack.git", revision: "a05d4bcf578430e8c5dbdae56cb6ac395cd806a0"),
